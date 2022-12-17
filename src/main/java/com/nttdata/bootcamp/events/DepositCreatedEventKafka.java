5dda0fb798d1414a89f72047e5c5a64a1fb47fe8
jhonatan-maza <jonathan.mazav31@gmail.com>
2022-12-16 19:27:26 -0500

    update

--- src/main/java/com/nttdata/bootcamp/events/DepositCreatedEventKafka.java
+++ src/main/java/com/nttdata/bootcamp/events/DepositCreatedEventKafka.java
@@ -6,6 +6,6 @@ import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
-public class CreatedEventKafka extends EventKafka<Deposit> {
+public class DepositCreatedEventKafka extends EventKafka<Deposit> {
 
 }
